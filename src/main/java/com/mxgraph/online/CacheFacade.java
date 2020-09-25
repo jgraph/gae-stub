d50ac1f000bbe105e02e4e2c7f7bedb4bd514574
Ashraf Teleb <ashraf.teleb85@gmail.com>
2020-09-25 16:12:46 -0400

    Changed CacheFacade to support memcached

--- src/main/java/com/mxgraph/online/CacheFacade.java
+++ src/main/java/com/mxgraph/online/CacheFacade.java
@@ -1,5 +1,8 @@
 package com.mxgraph.online;
 
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 import javax.cache.Cache;
 import javax.cache.CacheManager;
 import javax.cache.Caching;
@@ -8,19 +11,44 @@ import javax.cache.expiry.CreatedExpiryPolicy;
 import javax.cache.expiry.Duration;
 import javax.cache.spi.CachingProvider;
 
+import org.memcached.jcache.MemcachedCachingProvider;
+
 public class CacheFacade {
 
 	private CacheFacade() {}
 
-	public static Cache<String, String> createCache()
+	public static Cache<String, String> createCache(String name, int expirationDelta)
 	{
-		CachingProvider provider = Caching.getCachingProvider();  
-		CacheManager cacheManager = provider.getCacheManager();   
+		String memcachedEndpoint = System.getenv().get("DRAWIO_MEMCACHED_ENDPOINT");
+		CachingProvider provider;
+		CacheManager cacheManager;
+
+		if (memcachedEndpoint != null)
+		{
+			provider = Caching.getCachingProvider(MemcachedCachingProvider.class.getName());
+			Properties properties = provider.getDefaultProperties();
+			properties.setProperty("servers", memcachedEndpoint);
+			properties.setProperty(name + ".useSharedClientConnection", "true");
+			cacheManager = provider.getCacheManager(
+                provider.getDefaultURI(), null, properties);
+		}
+		else
+		{
+			provider = Caching.getCachingProvider();
+			cacheManager = provider.getCacheManager();
+		}
+		
 		MutableConfiguration<String, String> configuration =
 		    new MutableConfiguration<String, String>()  
 		        .setTypes(String.class, String.class)   
-		        .setStoreByValue(true)
-		        .setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(Duration.FIVE_MINUTES));  
-		return cacheManager.createCache("jCache", configuration);
+		        .setStoreByValue(false) //Memcached does not support store by value
+		        .setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(new Duration(TimeUnit.SECONDS, expirationDelta)));  
+		return cacheManager.createCache(name, configuration);
+	}
+
+	public static String getStatistics()
+	{
+		//TODO implement this for memcached
+		return "NYI";
 	}
 }
