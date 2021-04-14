913019a489e313dc92d2256efe1e716d44e0a9c1
David Benson [draw.io] <david@jgraph.com>
2021-04-14 15:51:42 +0100

    Added MemcacheServiceException

---
+++ src/main/java/com/google/appengine/api/memcache/MemcacheServiceException.java
@@ -0,0 +1,9 @@
+package com.google.appengine.api.memcache;
+
+public class MemcacheServiceException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6630777004051465616L;
+}
