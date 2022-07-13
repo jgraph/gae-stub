497c21d54d016eadbb0fe0056c7932370e13499a
Ashraf Teleb <ashraf.teleb85@gmail.com>
2022-07-13 16:46:36 +0200

    Fixed gae-stub

--- src/main/java/com/mxgraph/online/SecretFacade.java
+++ src/main/java/com/mxgraph/online/SecretFacade.java
@@ -1,6 +1,10 @@
 package com.mxgraph.online;
 
 import javax.servlet.ServletContext;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class SecretFacade 
 {
@@ -10,7 +14,7 @@ public class SecretFacade
     {
 		try
 		{
-			return Utils.readInputStream(servletContext
+			return readInputStream(servletContext
 							.getResourceAsStream("/WEB-INF/" + key))
 							.replaceAll("\n", "");
 		}
@@ -19,4 +23,29 @@ public class SecretFacade
 			throw new RuntimeException("Reading secret " + key + " failed.");
 		}
     }
+
+	/**
+	 * Reads an input stream and returns the result as a String
+	 * @param stream the input stream to read
+	 * @return a String representation of the input stream
+	 * @throws IOException
+	 */
+	public static String readInputStream(InputStream stream) throws IOException
+	{
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(stream));
+		StringBuffer result = new StringBuffer();
+		String tmp = reader.readLine();
+
+		while (tmp != null)
+		{
+			result.append(tmp + "\n");
+			tmp = reader.readLine();
+		}
+
+		reader.close();
+
+		return result.toString();
+	}
+
 }
